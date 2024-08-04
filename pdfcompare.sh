4f184cc9284e4739aedab493b5f0254cc2e7d164
Juergen Weigert <jnweiger@gmail.com>
2024-08-04 22:02:37 +0200

    fix: pdfcompare.sh never worked, as it did not make the files accessible from insied the docker container.

--- pdfcompare.sh
+++ pdfcompare.sh
@@ -3,7 +3,7 @@
 # (C) 2024 jnweiger@gmail.com - pdfcompare
 #
 # My old pdfcompare DEB package runs on nothing newer than Ubuntu 16.04
-# This script has instructions to ceate a docker image from that.
+# This script has instructions to create a docker image from that.
 #
 # As a docker image, it runs on any Linux.
 # TODO: Create a Dockerfile from this. Create an AppImage from this.
@@ -18,5 +18,28 @@
 # docker rm u1604pdf
 #
 
-docker run --rm pdfcompare pdfcompare "$@"
+tmpd=/tmp/pdfc$$dir
+outfile="$tmpd/output.pdf"
 
+mkdir -p $tmpd
+
+# find *.pdf arguments from the command line, copy them to /tmp/pdfc
+argv=()
+for arg in "$@"; do
+	case "$arg" in
+		*.pdf)
+			fname="$(basename "$arg")"
+			cp "$arg" "$tmpd/$fname" || { rm -rf "$tmpd"; exit 1; }
+			argv+=("$tmpd/$fname")
+			;;
+		*)
+			argv+=("$arg")
+			;;
+	esac
+done
+
+docker run --rm -v "$tmpd:$tmpd" pdfcompare pdfcompare -o "$outfile" "${argv[@]}"
+
+# copy output.pdf to the current working dir and delete /tmp/pdfc
+test -f "$outfile" && cp -b "$outfile" .
+rm -rf "$tmpd"
